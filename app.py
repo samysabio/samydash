7a45683561479d4bd31ea3584af99c3a6933fee7
samysabio <79102159+samysabio@users.noreply.github.com>
2021-05-28 03:32:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -62,8 +62,8 @@ server = app.server
 
 
 app.layout = html.Div([
-    html.Img(src=app.get_asset_url('IST.PNG')),
-    #html.Img(src=app.get_asset_url('IST.PNG'), style={"display": "flex",
+    #html.Img(src=app.get_asset_url('/assets/IST.PNG')),
+    html.Img(src=app.get_asset_url('/assets/IST.PNG'), style={"display": "flex",
                                "justifyContent": "center"}),
     html.H1('IST South Tower Energy Forecast Project', style={"display": "flex",
                                "justifyContent": "center"}),
@@ -240,13 +240,13 @@ def render_figure_html(dropdown_year):
 def render_figure_html(dropdown_yea):
     
     if dropdown_yea == 2060:
-        return html.Img(src=app.get_asset_url('ForestR.PNG'), width=600),
+        return html.Img(src=app.get_asset_url('ForestR.PNG'), width=1000),
            
     if dropdown_yea == 2061:
-        return html.Img(src=app.get_asset_url('TreeD1.PNG'), width=600),
+        return html.Img(src=app.get_asset_url('TreeD1.PNG'), width=1000),
     
     if dropdown_yea == 2062:
-        return html.Img(src=app.get_asset_url('VectorS.PNG'), width=600),
+        return html.Img(src=app.get_asset_url('VectorS.PNG'), width=1000),
 
 #Clean Data
 @app.callback(Output('graphyear1_html', 'children'), 
@@ -280,11 +280,11 @@ def render_figure_html(dropdown_ye):
 def render_figure_html(dropdown_y):
     
     if dropdown_y == 2040:
-        return html.Img(src=app.get_asset_url('Elbow Curve.PNG'), width=600),
+        return html.Img(src=app.get_asset_url('Elbow Curve.PNG'), width=1000),
     elif dropdown_y == 2041:
-        return html.Img(src=app.get_asset_url('Clustering kW - Hr.PNG'), width=600),
+        return html.Img(src=app.get_asset_url('Clustering kW - Hr.PNG'), width=1000),
     elif dropdown_y == 2042:
-        return html.Img(src=app.get_asset_url('Cluster Power Plot.PNG'), width=600),
+        return html.Img(src=app.get_asset_url('Cluster Power Plot.PNG'), width=1000),
 
     
 #Feature Selection
@@ -294,9 +294,9 @@ def render_figure_html(dropdown_y):
 def render_figure_html(dropdown_):
     
     if dropdown_ == 2050:
-        return html.Img(src=app.get_asset_url('FS - kBest.png'), width=600),
+        return html.Img(src=app.get_asset_url('/asset/FS - kBest.png'), width=1000),
     elif dropdown_ == 2051:
-        return html.Img(src=app.get_asset_url('FS - Table.png'), width=600),
+        return html.Img(src=app.get_asset_url('/asset/FS - Table.png'), width=1000),
 #
          
                       
